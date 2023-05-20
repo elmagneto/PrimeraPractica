545edc120df5e49f582539b5e552a9f07207c4bf
elMagneto <raul_toribio@yahoo.com>
2023-05-20 14:44:12 -0400

    Modificacion2

--- holamundo.c
+++ holamundo.c
@@ -5,11 +5,15 @@ void main(){
     char variable;
 
     printf("Hola mundo\n");
-    scanf("%c",variable);
+    scanf("%c",&variable);
 
     printf("%c Hola mundo\n",variable);
 
     printf("Segunda configuracion");
+
+
+    //comentarios de una nueva integracion.
+    printf("Segunda2 configuracion");
 
     
 }
