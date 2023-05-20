e62468290907689d38004206145f85e55129ee3b
elMagneto <raul_toribio@yahoo.com>
2023-05-20 14:37:57 -0400

    #comentario 1

---
+++ holamundo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(){
+
+    char variable;
+
+    printf("Hola mundo\n");
+    scanf("%c",variable);
+
+    printf("%c Hola mundo\n",variable);
+
+    printf("Segunda configuracion");
+
+    
+}
